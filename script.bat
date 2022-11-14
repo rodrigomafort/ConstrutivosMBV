d0c1b2ad408e8f474e8dbf52af78a0e0f023d7cd
AndersonBeltrao <anderson_beltrao@id.uff.br>
2022-11-14 12:06:03 -0300

    Atualização do script

--- script.bat
+++ script.bat
@@ -1,5 +1,5 @@
 @echo off
-echo File Branch_Vertices Time Verification > resultado.txt
+echo File Branch_Vertices Time Verification Branch_Vertices2 Time2 Verification2 > resultado.txt
 for /f %%f in ('dir /b Instancias\Spd_*.txt') do (	
 	bin\Release\ArvoreGeradora.exe Instancias\%%f >> resultado.txt
 )
