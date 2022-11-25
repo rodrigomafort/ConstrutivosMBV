8c8f845f02d3fde5b4c567ea6e6f05c50af2f33c
AndersonBeltrao <anderson_beltrao@id.uff.br>
2022-11-25 19:43:42 -0300

    Primeira heurística: Alteração na escolha do vértice v em Pontas considerando se o seu vizinho u está ou não na árvore

--- script2.bat
+++ script2.bat
@@ -1,5 +1,5 @@
 @echo off
-echo File Branch_Vertices Time Verification Branch_Vertices2 Time2 Verification2 > resultado2.txt
+echo File Branch_Vertices Time Verification Média_Grau_BV Branch_Vertices2 Time2 Verification2 Média_Grau_BV2 > resultado2.txt
 for /f %%f in ('dir /b Instancias2\Grafo_*.txt') do (	
 	bin\Release\ArvoreGeradora.exe Instancias2\%%f >> resultado2.txt
 )
