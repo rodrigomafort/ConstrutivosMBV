75348ba7eff9eaeb51771cc3c3ea357fc293287b
Rodrigo Mafort <mafort.rodrigo@gmail.com>
2022-12-01 10:16:16 -0300

    Adição do código da Roleta

---
+++ include/Roleta.h
@@ -0,0 +1,35 @@
+#ifndef ROLETA_H
+#define ROLETA_H
+
+#include <vector>
+#include <random>
+
+using namespace std;
+
+class ItemRoleta
+{
+    public:
+        double prob;
+        double probCalc;
+        double lowerBound;
+        double upperBound;
+        int key;
+
+        ItemRoleta(int key, double prob);
+};
+
+class Roleta
+{
+    private:
+        double soma;
+        vector<ItemRoleta> itens;
+
+    public:
+        Roleta();
+        ~Roleta();
+
+        void Adicionar(int key, double probalilidade);
+        int Sortear();
+};
+
+#endif // ROLETA_H
---
+++ src/Roleta.cpp
@@ -0,0 +1,54 @@
+#include "Roleta.h"
+
+ItemRoleta::ItemRoleta(int k, double p)
+{
+	key = k;
+	prob = p;
+	lowerBound = 0;
+	upperBound = 0;
+	probCalc = 0;
+}
+
+Roleta::Roleta()
+{
+	soma = 0;
+}
+
+Roleta::~Roleta()
+{
+}
+
+void Roleta::Adicionar(int key, double probabilidade)
+{
+	if (probabilidade < 0)
+		itens.push_back(ItemRoleta(key, 0));
+	else
+		itens.push_back(ItemRoleta(key, probabilidade));
+}
+
+int Roleta::Sortear()
+{
+	double soma = 0;
+	for (int i = 0; i < (int)itens.size(); i++)
+		soma = soma + itens[i].prob;
+
+	uniform_real_distribution<> rndInterval(0, 1);
+	random_device rd;
+	mt19937_64 e2(rd());
+	double d = rndInterval(e2);
+
+	double step = 0;
+	for (int i = 0; i < (int)itens.size(); i++)
+	{
+		itens[i].probCalc = itens[i].prob / soma;
+		itens[i].lowerBound = step;
+		itens[i].upperBound = step + itens[i].probCalc;
+		step = step + itens[i].probCalc;
+
+		if (d >= itens[i].lowerBound && d < itens[i].upperBound)
+		{
+			return itens[i].key;
+		}
+	}
+	return itens[itens.size() - 1].key;
+}
